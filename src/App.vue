1aa9fc1e59565e9c7be7ece0b12fcf430e1b9093
BC <sbc0830@osstem.com>
2019-05-03 10:36:05 +0900

    VueMaterial + 디렉토리 구조 + webWorker 의존

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+
+main {
+  text-align: center;
+}
+div {
+ background: red;
+
+}
+
+</style>
